861192faf829a4634cf2bdd79b042bd4cef6e3fd
GoldenStack <goldenfire64yt@gmail.com>
2024-01-15 02:16:11 -0600

    Remove blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,13 +48,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -80,7 +73,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/GoldenStack/maize',
             label: 'GitHub',
