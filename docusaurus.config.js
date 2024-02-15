34dd9231311abaa27fda457f7276e65b3f77efb9
GoldenStack <goldenfire64yt@gmail.com>
2024-02-15 17:43:19 -0600

    Fix GitHub pages references

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/maize-emoji.ico',
 
   // Set the production url of your site here
-  url: 'https://GoldenStack.github.io',
+  url: 'https://goldenstack.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/maize-docs/',
+  baseUrl: '/maize',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
