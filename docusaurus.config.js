f648798588d2ceb95a4563556ced29ddf1c627be
GoldenStack <goldenfire64yt@gmail.com>
2024-02-15 17:46:47 -0600

    Remove base URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/maize-emoji.ico',
 
   // Set the production url of your site here
-  url: 'https://goldenstack.net',
+  url: 'https://maize.goldenstack.net',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/maize',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
